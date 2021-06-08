e69db41a5e5b3e550dfd044b67161a86e44bfb0c
Greathoney <thyoon48@gmail.com>
2021-06-08 17:12:50 +0900

    구조 전체적 변경

--- PL/core.c
+++ PL/core.c
@@ -78,10 +78,12 @@ const int stage_size[2] = { 160, 20 };
 const int ball_position[2] = { 50, 86 };
 const int ball_size[2] = { 10, 10 };
 
-const int spike_position[2] = { 40, 86 };
+const int spike_position[2] = { 190, 86 };
 const int spike_size[2] = { 10, 10 };
+const int spike_path_length = 150;
 
 double jump_ball_speed = 2;
+double ball_gravity = 0.1
 
 /* Global Variables */
 int is_button_pushed[4] = { 0, 0, 0, 0 };
@@ -96,7 +98,10 @@ game_t games[4];
 
 /* Functions */
 void game_mode_0();
-void draw_square(int start_pos_X, int start_pos_Y, int length_X, int length_Y, rgb_t color);
+void game_mode_1();
+void game_mode_2();
+void draw_square(int, int, int, int, rgb_t);
+void redraw_square(int, int, int, int, rgb_t, int, int, int, int, rgb_t);
 int compile_rgb(rgb_t);
 void game_check(game_t *);
 void ball_jump_check(game_t *);
@@ -120,45 +125,20 @@ int main(void)
 
     /* tft lcd initlization */
 
-    // rgb_t ball_color;
-    // ball_color.R = ball_color_R;
-    // ball_color.G = ball_color_G;
-    // ball_color.B = ball_color_B;
-
-    // rgb_t stage_color;
-    // stage_color.R = stage_color_R;
-    // stage_color.G = stage_color_G;
-    // stage_color.B = stage_color_B;
-
-    // rgb_t background_color[4];
-    // for (int i = 0; i < 4; i++){
-    //     background_color[i].R = background_color_R_mode1[i];
-    //     background_color[i].G = background_color_G_mode1[i];
-    //     background_color[i].B = background_color_B_mode1[i];
-    // }
-
     /* Game Loop */
-
     while (1) {
         if (game_mode == 0) {	
 			game_mode_0();
         }
-
         else if (game_mode == 1) {
 			game_mode_1();
         }
         else {  // game_mode == 2
-            /* Game Logic */
-
-            /* text lcd Display */
-
-            /* tft lcd Display */
+			game_mode_2();
         }
-
 		for (int i = 0; i < 4; i++){
 			is_button_pushed[i] = 0;
 		}
-
         usleep(fps_delay);     // 1/30 sec (=33msec)
     }
     return 0;
@@ -292,31 +272,31 @@ void game_mode_1(){
 	playtime = ( (playtime & 0x00000F00) >> 8 )*60 +( (playtime & 0x000000F0) >> 4 )*10 +(playtime & 0x0000000F);
 	xil_printf("playtime : (16 radix)%xsec (10 radix)%dsec \r\n",playtime);
 
-	switch (game_count){	// stage position for total game number
-		case 1:
-			games[0].stage_x_position = (DISPLAY_WIDTH - STAGE_WIDTH) / 2;
-			games[0].stage_y_position = (DISPLAY_HEIGHT - STAGE_HEIGHT) / 2;
-			break;
-		case 2:
-			games[0].stage_x_position = ((DISPLAY_WIDTH / 2) - STAGE_WIDTH)) / 2;
-			games[1].stage_x_position = (DISPLAY_WIDTH - (DISPLAY_WIDTH / 4)) - (STAGE_WIDTH / 2);
-			games[1].stage_y_position = (DISPLAY_HEIGHT - STAGE_HEIGHT) / 2;
-			break;
-		case 3:
-			games[0].stage_y_position = (DISPLAY_HEIGHT / 4) - (STAGE_HEIGHT / 2);
-			games[2].stage_x_position = (DISPLAY_WIDTH - STAGE_WIDTH) / 2;
-			games[2].stage_y_position = (DISPLAY_HEIGHT - (DISPLAY_HEIGHT / 4)) - (STAGE_HEIGHT / 2);
-			break;
-		case 4:
-			games[1].stage_y_position = (DISPLAY_HEIGHT / 4) - (STAGE_HEIGHT / 2);
-			games[3].stage_x_position = (DISPLAY_WIDTH - (DISPLAY_WIDTH / 4)) - (STAGE_WIDTH / 2);
-			games[3].stage_y_position = (DISPLAY_HEIGHT - (DISPLAY_HEIGHT / 4)) - (STAGE_HEIGHT / 2);
-			break;
-		default:
-			break;
-	}			
+	// switch (game_count){	// stage position for total game number
+	// 	case 1:
+	// 		games[0].stage_x_position = (DISPLAY_WIDTH - STAGE_WIDTH) / 2;
+	// 		games[0].stage_y_position = (DISPLAY_HEIGHT - STAGE_HEIGHT) / 2;
+	// 		break;
+	// 	case 2:
+	// 		games[0].stage_x_position = ((DISPLAY_WIDTH / 2) - STAGE_WIDTH)) / 2;
+	// 		games[1].stage_x_position = (DISPLAY_WIDTH - (DISPLAY_WIDTH / 4)) - (STAGE_WIDTH / 2);
+	// 		games[1].stage_y_position = (DISPLAY_HEIGHT - STAGE_HEIGHT) / 2;
+	// 		break;
+	// 	case 3:
+	// 		games[0].stage_y_position = (DISPLAY_HEIGHT / 4) - (STAGE_HEIGHT / 2);
+	// 		games[2].stage_x_position = (DISPLAY_WIDTH - STAGE_WIDTH) / 2;
+	// 		games[2].stage_y_position = (DISPLAY_HEIGHT - (DISPLAY_HEIGHT / 4)) - (STAGE_HEIGHT / 2);
+	// 		break;
+	// 	case 4:
+	// 		games[1].stage_y_position = (DISPLAY_HEIGHT / 4) - (STAGE_HEIGHT / 2);
+	// 		games[3].stage_x_position = (DISPLAY_WIDTH - (DISPLAY_WIDTH / 4)) - (STAGE_WIDTH / 2);
+	// 		games[3].stage_y_position = (DISPLAY_HEIGHT - (DISPLAY_HEIGHT / 4)) - (STAGE_HEIGHT / 2);
+	// 		break;
+	// 	default:
+	// 		break;
+	// }			
 
-	// button check
+	// TODO: 시간이 다 되어 game_count가 변해야 하는지 체크, is_background_paint = 0으로 만들어주는 구문 작성
 
 	for (int i = 0; i < 4; i++){
 		if (games[i].is_game_exist) {
@@ -328,9 +308,24 @@ void game_mode_1(){
 
 	/* tft lcd Display */
 	if (is_background_paint == 0){
+		// game_mode 또는 game_count 가 바뀌어 완전히 새로 그리게 될 때 여기서 한번 전체적으로 그려주게 된다.
+		// TODO: 현재 선언된 변수와 함수로 그리기
 
 		is_background_paint = 1;
 	}
+}
+
+void game_mode_2(){
+	// TODO: mode 2 구현하기
+	// 게임 화면은 그대로 멈추게 되고, txtlcd를 통해서 점수나 현재 상황을 표시합니다.
+	// 그리고 버튼을 누르면 game mode 0 로 돌아가게 됩니다.
+
+	/* Game Logic */
+
+	/* text lcd Display */
+
+	/* tft lcd Display */
+
 }
 
 void draw_square(int start_pos_X, int start_pos_Y, int length_X, int length_Y, rgb_t color){
@@ -341,15 +336,29 @@ void draw_square(int start_pos_X, int start_pos_Y, int length_X, int length_Y, r
 	}
 }
 
+void redraw_square(int draw_start_pos_X, int draw_draw_start_pos_Y, int draw_length_X, int draw_length_Y, rgb_t draw_color,  // 새롭게 그릴 사각형
+				   int delete_start_pos_X, int delete_draw_start_pos_Y, int delete_length_X, int delete_length_Y, rgb_t background_color){  // 이미 그려져서 배경화면 색으로 덮을 사각형
+
+	draw_square(delete_start_pos_X, delete_draw_start_pos_Y, delete_length_X, delete_length_Y, rgb_t background_color);
+	draw_square(draw_start_pos_X, draw_draw_start_pos_Y, draw_length_X, draw_length_Y, rgb_t draw_color)
+
+}
+
 int compile_rgb(rgb_t rgb_data){
     return (rgb_data.B << 11 | rgb_data.G << 5 | rgb_data.R);
 }
 
 void game_check(game_t *game){
-	ball_jump_check(game);
 	is_spike_touched(game);
+	ball_jump_check(game);
 	change_ball_position(game);
-	generate_spike(game);
+	if (!game->is_game_exist){
+		generate_spike(game);
+	}
+	else {
+		remove_spike(game);
+	}
+
 	change_spike_position(game);
 }
 
@@ -361,19 +370,22 @@ void ball_jump_check(game_t *game){
 }
 
 void is_spike_touched(game_t *game){
-	if( ((game->stage_x_position + BALL_OFFSET - BALL_SIZE) <= game->spike_x_position < (game->stage_x_position + BALL_OFFSET + BALL_SIZE))
-		&& (game->ball_y_position + BALL_SIZE) > (game->stage_y_position - SPIKE_SIZE)){
-		game_mode = 2;
-	}
+	// TODO: 현재 선언한 변수로 논의된 로직으로 코드 작성 부탁드립니다.
+	// if( ((game->stage_x_position + BALL_OFFSET - BALL_SIZE) <= game->spike_x_position < (game->stage_x_position + BALL_OFFSET + BALL_SIZE))
+	// 	&& (game->ball_y_position + BALL_SIZE) > (game->stage_y_position - SPIKE_SIZE)){
+	// 	game_mode = 2;
 }
 
 void change_ball_position(game_t *game){
 	if(game->is_ball_jumping){
-		game->ball_y_position -= game->ball_y_speed;
-		game->ball_y_speed -= 0.1;
-		// if(game->ball_y_position <= (game->stage_y_position - 15)){
-		// 	game->ball_y_speed *= (-1);
-		// };
+		game->ball_y_position += game->ball_y_speed;
+		// 만일 ball position이 한 픽셀 움직이게 될 경우 해당 구역만 지우고 다시 그린다.
+		// TODO: 조건문과 입력 변수 채워 넣기
+		if (0) {
+			redraw_sqaure()
+		}
+		game->ball_y_speed -= ball_gravity;
+
 		else if(game->ball_y_position >= game->stage_y_position){
 			game->ball_y_position = game->stage_y_position
 			game->is_ball_jumping = 0;
@@ -382,18 +394,33 @@ void change_ball_position(game_t *game){
 }
 
 void generate_spike(game_t *game){
-	if(!game->is_spike_exist){
-		game->is_spike_exist = ((rand()%2) == 1));
-		game->spike_x_position = game->stage_x_position + STAGE_WIDTH - SPIKE_SIZE;
-		game->spike_x_speed = 0.7;
-	}
+	// TODO: 가시 생성 아래 주석을 참고하여 코드 작성 부탁드립니다.
+	// 가시는 랜덤하게 생성함
+	// 가시의 속도도 랜덤
+
+	// 아래 조건문은 상위 함수에서 작성함
+	// if(!game->is_spike_exist){
+	// 	game->is_spike_exist = ((rand()%2) == 1));
+	// 	game->spike_x_position = game->stage_x_position + STAGE_WIDTH - SPIKE_SIZE;
+	// 	game->spike_x_speed = 0.7;
+	// }
+}
+
+void remove_spike(game_t *game){
+	// spike_x_position은 계속해서 작아져서 spike_path_length(현재 150) 만큼 가게 되고 이 이후로는 가시가 사라지게 됩니다.
+	// TODO: 현재 선언된 변수를 이용하여 다시 작성부탁드립니다.
+	// if(game->spike_x_position < game->stage_x_position){
+	// 	game->is_spike_exist = 0;
+	// }
 }
 
 void change_spike_position(game_t *game){
-	if(game->is_spike_exist){
-		game->spike_x_position -= game->spike_x_speed;
-		if(game->spike_x_position < game->stage_x_position){
-			game->is_spike_exist = 0;
-		}
+	// spike_x_position은 0에서 시작하여 계속해서 작아집니다.
+
+	game->spike_x_position -= game->spike_x_speed;
+	// 만일 spike position이 한 픽셀 움직이게 될 경우 해당 구역만 지우고 다시 그린다.
+	// TODO: 조건문과 입력 변수 채워 넣기
+	if (0) {
+		redraw_sqaure()
 	}
 }
